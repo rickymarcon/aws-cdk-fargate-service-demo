08eeadb8f08831931ea502c4f312f736a1a5048c
Ricky Marcon <ricky.marcon@gmail.com>
2020-06-03 14:51:56 +0100

    feat: add ecs cluster with ALB fargate service

--- lib/ecs-cluster-stack.ts
+++ lib/ecs-cluster-stack.ts
@@ -1,9 +1,34 @@
 import * as cdk from '@aws-cdk/core';
+import * as ec2 from '@aws-cdk/aws-ec2';
+import * as ecs from '@aws-cdk/aws-ecs';
+import * as ecsPatterns from '@aws-cdk/aws-ecs-patterns';
 
 export class EcsClusterStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, 'NamedVPC', {
+      maxAzs: 3,
+    });
+
+    const cluster = new ecs.Cluster(this, 'NamedCluster', {
+      vpc,
+    });
+
+    // Create a load-balanced Fargate service and make it public
+    new ecsPatterns.ApplicationLoadBalancedFargateService(
+      this,
+      'NamedFargateService',
+      {
+        cluster,
+        cpu: 512, // Default is 256
+        desiredCount: 6,
+        taskImageOptions: {
+          image: ecs.ContainerImage.fromRegistry('amazon/amazon-ecs-sample'),
+        },
+        memoryLimitMiB: 2048, // Default is 512,
+        publicLoadBalancer: true,
+      }
+    );
   }
 }
