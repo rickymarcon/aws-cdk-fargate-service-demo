9084be56fc3edb165278fa911c1a20d4cd1849d0
Ricky Marcon <ricky.marcon@gmail.com>
2020-06-03 21:43:19 +0100

    test(cdk): add tests for elb, serivce and task defs

--- cdk/test/ecs-cluster.test.ts
+++ cdk/test/ecs-cluster.test.ts
@@ -24,4 +24,43 @@ describe('CDK ECS cluster', () => {
   test('creates an ECS cluster', () => {
     expectCDK(stack).to(haveResource('AWS::ECS::Cluster'));
   });
+
+  test('creates an ELB', () => {
+    expectCDK(stack).to(
+      haveResource('AWS::ElasticLoadBalancingV2::LoadBalancer')
+    );
+    expectCDK(stack).to(haveResource('AWS::ElasticLoadBalancingV2::Listener'));
+    expectCDK(stack).to(
+      haveResource('AWS::ElasticLoadBalancingV2::TargetGroup')
+    );
+  });
+
+  test('allows ingress to cluster from 0.0.0.0/0', () => {
+    expectCDK(stack).to(
+      haveResource('AWS::EC2::SecurityGroup', {
+        SecurityGroupIngress: [
+          {
+            CidrIp: '0.0.0.0/0',
+            Description: 'Allow from anyone on port 80',
+            FromPort: 80,
+            IpProtocol: 'tcp',
+            ToPort: 80,
+          },
+        ],
+      })
+    );
+  });
+
+  test('creates a task definition', () => {
+    expectCDK(stack).to(haveResource('AWS::ECS::TaskDefinition'));
+  });
+
+  test('service is created', () => {
+    expectCDK(stack).to(
+      haveResource('AWS::ECS::Service', {
+        LaunchType: 'FARGATE',
+        DesiredCount: 1,
+      })
+    );
+  });
 });
